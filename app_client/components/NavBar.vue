0d1ce19922651cfe71e24f26d148db2080fa4ab5
Kleinne <renatasgal420@gmail.com>
2021-10-31 16:45:47 +0200

    add inertia links to navbar

--- app_client/components/NavBar.vue
+++ app_client/components/NavBar.vue
@@ -1,21 +1,18 @@
 <script setup name="NavBar">
-defineProps({
-  links: {
-    type: Array,
-    default: null,
-  },
-});
+/* can't access route in script setup */
 </script>
 
 <template>
   <div
-    class="fixed inset-x-0 top-0 flex items-center w-full h-12 space-x-10 bg-lred-500/60"
+    class="fixed inset-x-0 top-0 flex items-center w-full h-12 space-x-10 bg-soft-500"
   >
-    <div class="ml-80">immersion hub</div>
-    <div>Home</div>
-    <div>Reviews</div>
-    <div>Recommendations</div>
-    <div>Community</div>
-    <InertiaLink :href="route('app.contribute')">Contribute</InertiaLink>
+    <InertiaLink :href="route('app.home')" class="ml-80">
+      immersion hub
+    </InertiaLink>
+    <InertiaLink :href="route('app.home')">Home</InertiaLink>
+    <InertiaLink :href="route('app.home')">Reviews</InertiaLink>
+    <InertiaLink :href="route('app.home')">Recommendations</InertiaLink>
+    <InertiaLink :href="route('app.home')">Community</InertiaLink>
+    <InertiaLink :href="route('contribute.index')">Contribute</InertiaLink>
   </div>
 </template>

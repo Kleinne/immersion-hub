7828dd882fbb82189149619c49819d90749cc7e3
Kleinne <renatasgal420@gmail.com>
2021-11-30 13:26:24 +0200

    create base card

---
+++ app_client/components/BaseCard.vue
@@ -0,0 +1,15 @@
+<script setup name="BaseCard">
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <div class="p-10 bg-white rounded-md shadow-idle">
+    <h1 class="mb-10 text-lg font-semibold">{{ title }}</h1>
+    <slot />
+  </div>
+</template>

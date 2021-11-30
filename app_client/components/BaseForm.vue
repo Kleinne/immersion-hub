3a1b3f99e1caf14bb40ebccc247c52edd200da15
Kleinne <renatasgal420@gmail.com>
2021-11-30 13:26:35 +0200

    create base form

---
+++ app_client/components/BaseForm.vue
@@ -0,0 +1,21 @@
+<script setup name="BaseForm">
+import BaseButton from './BaseButton.vue';
+
+defineProps({
+  action: {
+    type: String,
+    default: 'POST',
+    validator: (value) => ['POST', 'GET'].includes(value),
+  },
+});
+</script>
+
+<template>
+  <form :action="action" class="max-w-lg">
+    <slot />
+
+    <div class="flex justify-end">
+      <BaseButton class="mt-5 min-w-[100px]">Submit</BaseButton>
+    </div>
+  </form>
+</template>

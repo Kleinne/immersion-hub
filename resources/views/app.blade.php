2c501c55718da11950983a8ad643aa4d6b02928c
Kleinne <renatasgal420@gmail.com>
2021-11-30 13:25:44 +0200

    default font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,12 @@
     <script type="module" src="http://localhost:3030/@vite/client"></script>
     <script type="module" src="http://localhost:3030/app_client/index.js"></script>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet" /> -->
+
     @routes
 </head>
 

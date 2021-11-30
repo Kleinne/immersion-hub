34b46a3792f3c5186d92df630873b4c7c7df6216
Kleinne <renatasgal420@gmail.com>
2021-11-30 13:52:57 +0200

    add form cols

---
+++ app_client/components/FormCols.vue
@@ -0,0 +1,14 @@
+<script setup name="FormCols">
+defineProps({
+  single: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div class="flex" :class="{ 'space-x-6': !single }">
+    <slot />
+  </div>
+</template>
--- app_client/pages/ContributeIndex.vue
+++ app_client/pages/ContributeIndex.vue
@@ -4,6 +4,7 @@ import { useContributeForm } from '../features/useContributeForm.js';
 import FormInput from '../components/FormInput.vue';
 import BaseCard from '../components/BaseCard.vue';
 import BaseForm from '../components/BaseForm.vue';
+import FormCols from '../components/FormCols.vue';
 
 // const props = defineProps({});
 const { form, schema } = useContributeForm();
@@ -18,7 +19,7 @@ const { form, schema } = useContributeForm();
       <FormInput v-model="form.volumeTitleEn" v-bind="schema.volumeTitleEn" />
       <FormInput v-model="form.titleRo" v-bind="schema.titleRo" />
       <FormInput v-model="form.volumeTitleRo" v-bind="schema.volumeTitleRo" />
-      <div class="flex space-x-6">
+      <FormCols>
         <FormInput
           class="max-w-[150px]"
           v-model="form.volume"
@@ -30,7 +31,7 @@ const { form, schema } = useContributeForm();
           v-bind="schema.pages"
         />
         <FormInput class="flex-1" v-model="form.date" v-bind="schema.date" />
-      </div>
+      </FormCols>
       <FormInput v-model="form.author" v-bind="schema.author" />
       <FormInput v-model="form.authorEn" v-bind="schema.authorEn" />
       <FormInput v-model="form.illustrator" v-bind="schema.illustrator" />

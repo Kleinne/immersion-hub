693dc7e00098dafdc4533f4619cfca1f6777ab4d
Kleinne <renatasgal420@gmail.com>
2021-12-31 17:25:00 +0200

    add useVModel

--- app_client/features/useVModel.js
+++ app_client/features/useVModel.js
@@ -6,3 +6,25 @@ export const useVModelProps = (type = defaultTypes, defaultValue = null) => ({
     default: defaultValue,
   },
 });
+
+export const useVModel = (props, emit) => {
+  const inputValue = ref(props.modelValue);
+
+  watch(
+    () => inputValue.value,
+    (value) => {
+      emit('update:modelValue', value);
+    },
+    { deep: true },
+  );
+
+  watch(
+    () => props.modelValue,
+    (value) => {
+      inputValue.value = value;
+    },
+    { deep: true },
+  );
+
+  return { inputValue };
+};

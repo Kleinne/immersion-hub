4cfe896380fdc572df29f1c87fbe4233c898c93e
Kleinne <renatasgal420@gmail.com>
2022-01-03 13:25:54 +0200

    add error state to select field

--- app_client/components/FormSelectField.vue
+++ app_client/components/FormSelectField.vue
@@ -8,30 +8,41 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+  errors: {
+    type: String,
+    default: null,
+  },
 });
 
 const { inputValue } = useVModel(props, emit);
 </script>
 <template>
-  <label class="flex items-center justify-start" :for="field.name">
-    <span class="text-sm min-w-[150px]">
+  <label class="flex" :for="field.name">
+    <span class="text-sm min-w-[150px] pt-0.5">
       {{ field.label }}
       <span v-if="field.required" class="text-red-500">*</span>
     </span>
 
-    <select
-      v-model="inputValue"
-      class="w-1/4 py-0.5 text-sm border border-transparent rounded outline-none shadow-input-idle hover:border-violet-500 focus:border-violet-500 hover:shadow-input-hover focus:shadow-input-focus"
-      :id="field.name"
-      :name="field.name"
-    >
-      <option
-        v-for="option in field.options"
-        :value="option"
-        :key="option.name"
+    <div class="w-full">
+      <select
+        v-model="inputValue"
+        class="py-0.5 pr-10 text-sm border border-transparent rounded outline-none shadow-input-idle"
+        :class="{
+          'shadow-red-500': errors,
+          'hover:border-violet-500 focus:border-violet-500 hover:shadow-input-hover focus:shadow-input-focus': !errors,
+        }"
+        :id="field.name"
+        :name="field.name"
       >
-        {{ option.name }}
-      </option>
-    </select>
+        <option
+          v-for="option in field.options"
+          :value="option.value"
+          :key="option.name"
+        >
+          {{ option.name }}
+        </option>
+      </select>
+      <p v-if="errors" class="mt-1 text-xs text-red-500">{{ errors }}</p>
+    </div>
   </label>
 </template>

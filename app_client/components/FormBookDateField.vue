fb897b8d3ab4110b07e225c013a911b71c5c6b20
Kleinne <renatasgal420@gmail.com>
2022-01-03 16:50:58 +0200

    book date field errors

--- app_client/components/FormBookDateField.vue
+++ app_client/components/FormBookDateField.vue
@@ -8,51 +8,78 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+  errors: {
+    type: String,
+    default: null,
+  },
 });
 
 const { inputValue } = useVModel(props, emit);
 </script>
 <template>
-  <div class="flex space-x-2">
-    <label class="flex items-center justify-start" :for="field.name">
-      <span class="text-sm min-w-[150px]">
+  <div class="flex">
+    <label class="flex" :for="field.name">
+      <span class="text-sm min-w-[150px] pt-0.5">
         {{ field.label }}
       </span>
-
-      <span class="mr-2 text-sm">Year:</span>
-      <input
-        v-model="inputValue.year"
-        class="px-2 w-20 py-0.5 text-sm border border-transparent rounded outline-none shadow-input-idle hover:border-violet-500 focus:border-violet-500 hover:shadow-input-hover focus:shadow-input-focus"
-        name="year"
-        :id="field.name"
-        type="number"
-      />
     </label>
 
-    <label class="flex items-center justify-start" for="month">
-      <span class="mr-2 text-sm">Month:</span>
-      <select
-        v-model="inputValue.month"
-        class="w-[100px] py-0.5 text-sm border border-transparent rounded outline-none shadow-input-idle hover:border-violet-500 focus:border-violet-500 hover:shadow-input-hover focus:shadow-input-focus"
-        id="month"
-        name="month"
-      >
-        <option value="" selected disabled>-</option>
-        <option v-for="month in field.months" :value="month" :key="month.name">
-          {{ month.name }}
-        </option>
-      </select>
-    </label>
+    <div>
+      <div class="flex space-x-2">
+        <label class="flex items-center justify-start" for="year">
+          <span class="mr-2 text-sm">Year:</span>
+          <input
+            v-model="inputValue.year"
+            class="px-2 w-20 py-0.5 text-sm border border-transparent rounded outline-none shadow-input-idle"
+            :class="{
+              'shadow-red-500': errors,
+              'hover:border-violet-500 focus:border-violet-500 hover:shadow-input-hover focus:shadow-input-focus': !errors,
+            }"
+            name="year"
+            :id="field.name"
+            type="number"
+          />
+        </label>
 
-    <label class="flex items-center justify-start" for="day">
-      <span class="mr-2 text-sm">Day:</span>
-      <input
-        v-model="inputValue.day"
-        class="px-2 py-0.5 w-[102px] text-sm border border-transparent rounded outline-none shadow-input-idle hover:border-violet-500 focus:border-violet-500 hover:shadow-input-hover focus:shadow-input-focus"
-        name="day"
-        id="day"
-        type="number"
-      />
-    </label>
+        <label class="flex items-center justify-start" for="month">
+          <span class="mr-2 text-sm">Month:</span>
+          <select
+            v-model="inputValue.month"
+            class="w-[100px] py-0.5 text-sm border border-transparent rounded outline-none shadow-input-idle"
+            :class="{
+              'shadow-red-500': errors,
+              'hover:border-violet-500 focus:border-violet-500 hover:shadow-input-hover focus:shadow-input-focus': !errors,
+            }"
+            id="month"
+            name="month"
+          >
+            <option value="" selected disabled>-</option>
+            <option
+              v-for="month in field.months"
+              :value="month"
+              :key="month.name"
+            >
+              {{ month.name }}
+            </option>
+          </select>
+        </label>
+
+        <label class="flex items-center justify-start" for="day">
+          <span class="mr-2 text-sm">Day:</span>
+          <input
+            v-model="inputValue.day"
+            class="px-2 py-0.5 w-[102px] text-sm border border-transparent rounded outline-none shadow-input-idle"
+            :class="{
+              'shadow-red-500': errors,
+              'hover:border-violet-500 focus:border-violet-500 hover:shadow-input-hover focus:shadow-input-focus': !errors,
+            }"
+            name="day"
+            id="day"
+            type="number"
+          />
+        </label>
+      </div>
+      <p v-if="errors" class="mt-1 text-xs text-red-500">{{ errors }}</p>
+    </div>
   </div>
 </template>

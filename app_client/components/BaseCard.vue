a84619d8443f9d165b7b33ea61814dcd7eba5987
Kleinne <renatasgal420@gmail.com>
2022-01-05 17:07:22 +0200

    reduce base card padding

--- app_client/components/BaseCard.vue
+++ app_client/components/BaseCard.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="p-10 shadow-md rounded-xl">
+  <div class="p-5 rounded-md shadow-md">
     <div class="mb-10 space-y-2">
       <h1 class="text-lg font-semibold" v-if="title">{{ title }}</h1>
       <h5 class="text-sm" v-if="subtitle">{{ subtitle }}</h5>

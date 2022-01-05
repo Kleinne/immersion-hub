8fe5f54cf1a40926714ea7348323864795ea3d15
Kleinne <renatasgal420@gmail.com>
2022-01-05 15:44:36 +0200

    remove useless links

--- app_client/components/NavBar.vue
+++ app_client/components/NavBar.vue
@@ -16,27 +16,6 @@
     </InertiaLink>
     <InertiaLink
       class="font-bold hover:text-ired-500"
-      :class="{ 'text-ired-500': $page.url.startsWith('/reviews') }"
-      :href="route('app.home')"
-    >
-      Reviews
-    </InertiaLink>
-    <InertiaLink
-      class="font-bold hover:text-ired-500"
-      :class="{ 'text-ired-500': $page.url.startsWith('/recommendations') }"
-      :href="route('app.home')"
-    >
-      Recommendations
-    </InertiaLink>
-    <InertiaLink
-      class="font-bold hover:text-ired-500"
-      :class="{ 'text-ired-500': $page.url.startsWith('/community') }"
-      :href="route('app.home')"
-    >
-      Community
-    </InertiaLink>
-    <InertiaLink
-      class="font-bold hover:text-ired-500"
       :class="{ 'text-ired-500': $page.url.startsWith('/books/new') }"
       :href="route('books.new')"
     >

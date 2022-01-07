2fa59ad8fb6644376f1d9bcd2e715fd50b399205
Kleinne <renatasgal420@gmail.com>
2022-01-07 16:51:49 +0200

    add preliminary guest layout

---
+++ app_client/layouts/GuestLayout.vue
@@ -0,0 +1,16 @@
+<script setup name="GuestLayout">
+import Footer from '../components/Footer.vue';
+import NavBar from '../components/NavBar.vue';
+</script>
+
+<template>
+  <div class="w-full h-screen overflow-y-auto">
+    <NavBar />
+    <main class="px-5 pt-24 md:px-40">
+      <div class="max-w-4xl mx-auto">
+        <slot />
+      </div>
+    </main>
+    <Footer class="mt-40" />
+  </div>
+</template>

9f2780ae2d398a9f682874bbfe8b6fffa14a37bd
Kleinne <renatasgal420@gmail.com>
2022-01-07 16:50:06 +0200

    add dynamic user to inertia props

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
@@ -37,9 +38,11 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            'auth' => [
-                'user' => []
-            ]
+            'auth' => Auth::user() ? [
+                'user' => [
+                    'username' => Auth::user()->username,
+                ]
+            ] : null,
         ]);
     }
 }

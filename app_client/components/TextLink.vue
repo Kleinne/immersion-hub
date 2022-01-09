0d323e483fe87c8130af2c53b06b3fb8d3a31cea
Kleinne <renatasgal420@gmail.com>
2022-01-09 08:51:06 +0200

    add text link component

---
+++ app_client/components/TextLink.vue
@@ -0,0 +1,16 @@
+<script setup name="TextLink">
+defineProps({
+  href: {
+    type: String,
+    default: null,
+  },
+});
+</script>
+<template>
+  <InertiaLink
+    :href="href"
+    class="transition-all text-ired-500 hover:text-ired-500/70"
+  >
+    <slot />
+  </InertiaLink>
+</template>

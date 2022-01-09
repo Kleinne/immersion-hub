edceabd00049eee309ca6342935cb91414cb801d
Kleinne <renatasgal420@gmail.com>
2022-01-09 08:50:48 +0200

    add base card header slot

--- app_client/components/BaseCard.vue
+++ app_client/components/BaseCard.vue
@@ -14,8 +14,10 @@ defineProps({
 <template>
   <section class="flex flex-col p-5 rounded-md shadow-md">
     <div class="mb-10 space-y-2">
-      <h1 class="text-lg font-semibold" v-if="title">{{ title }}</h1>
-      <h5 class="text-sm" v-if="subtitle">{{ subtitle }}</h5>
+      <slot name="header">
+        <h2 class="text-lg font-semibold" v-if="title">{{ title }}</h2>
+        <h5 class="text-sm" v-if="subtitle">{{ subtitle }}</h5>
+      </slot>
     </div>
     <slot />
   </section>

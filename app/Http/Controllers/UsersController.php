93e0f06b0c7676d274c5493c71d4805d5fdec70c
Kleinne <renatasgal420@gmail.com>
2022-01-13 16:42:53 +0200

    add user stats to profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,11 +8,27 @@ class UsersController extends Controller
 {
     public function show(User $user)
     {
+        $books = $user->books;
+        $pages = $books->where('pivot.status', 'completed')->sum('pages');
+
         return inertia('UsersShow', [
             'user' => [
                 'username' => $user->username,
-                'created_at' => $user->created_at,
-            ]
+            ],
+            'books' => [
+                'pages' => $pages,
+                'reading' => $this->getByStatus($books, 'reading'),
+                'completed' => $this->getByStatus($books, 'completed'),
+                'planned' => $this->getByStatus($books, 'planned'),
+            ],
+        ]);
+    }
+
+    function getByStatus(object $books, string $status)
+    {
+        return $books->where('pivot.status', $status)->values()->map(fn ($book) => [
+            'id' => $book->id,
+            'title' => $book->title,
         ]);
     }
 }
--- app_client/pages/UsersShow.vue
+++ app_client/pages/UsersShow.vue
@@ -1,15 +1,57 @@
 <script setup name="UsersShow">
+import BaseCard from '../components/BaseCard.vue';
+
 const props = defineProps({
   user: {
     type: Object,
     required: true,
   },
+  books: {
+    type: Object,
+    default: null,
+  },
 });
+
+const statsItems = computed(() => [
+  {
+    label: 'Reading',
+    value: props.books?.reading?.length,
+  },
+  {
+    label: 'Completed',
+    value: props.books?.completed?.length,
+  },
+  {
+    label: 'Planned',
+    value: props.books?.planned?.length,
+  },
+  {
+    label: 'Pages read',
+    value: props.books?.pages,
+  },
+]);
 </script>
 
 <template>
   <div>
     <InertiaHead :title="`${user.username}'s Profile`" />
-    {{ user.username }}
+
+    <div class="flex w-full space-x-3">
+      <BaseCard class="w-1/3" :title="`${user.username}`">
+        <div class="divide-y">
+          <h5 class="mb-2 font-bold">User's data</h5>
+          <div
+            class="flex items-center justify-between py-2"
+            v-for="{ label, value } in statsItems"
+            :key="label"
+          >
+            <p>{{ label }}</p>
+            <p>{{ value }}</p>
+          </div>
+        </div>
+      </BaseCard>
+
+      <BaseCard class="flex-1"></BaseCard>
+    </div>
   </div>
 </template>

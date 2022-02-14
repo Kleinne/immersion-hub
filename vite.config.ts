b82d55f21a20c3fca73d39d282f0b088f9453005
Kleinne <renatasgal420@gmail.com>
2022-02-14 15:24:08 +0200

    clean vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? '/dist/' : '',
-    publicDir: false,
-    build: {
-      manifest: true,
-      outDir: 'public/dist',
-      rollupOptions: {
-        input: {
-          app: 'app_client',
-        },
-      },
-    },
-    // TODO configure eslint
-    // resolve: {
-    //   alias: {
-    //     '@': path.resolve(__dirname, '/app_client'),
-    //   },
-    // },
-    server: {
-      strictPort: true,
-      port: 3030,
-      // https: true,
-      hmr: {
-        host: 'localhost',
-      },
-    },
-    plugins: [vue()],
-    optimizeDeps: {
-      include: ['@inertiajs/inertia', '@inertiajs/inertia-vue3', 'vue'],
-    },
-  };
+export default defineConfig({
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ['@inertiajs/inertia', '@inertiajs/inertia-vue3', 'vue'],
+  },
 });

df6664dd9b30736648bbb3e91e2181fcc4c40663
Kleinne <renatasgal420@gmail.com>
2022-02-14 15:25:39 +0200

    change app.blade port

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
-    <script type="module" src="http://localhost:3030/@vite/client"></script>
-    <script type="module" src="http://localhost:3030/app_client/index.ts"></script>
+    <script type="module" src="http://localhost:3000/@vite/client"></script>
+    <script type="module" src="http://localhost:3000/app_client/index.ts"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
